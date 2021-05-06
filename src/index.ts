bbf25fb63b89615eda1f6f3021b37ba0431adca6
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2021-05-05 21:53:45 -0600

    Improve bootstraping api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import Express from 'express';
-import http from 'http';
+import Express, { Request, Response } from 'express';
 
+import dotenv from 'dotenv';
+
+import loadMiddlewares from './middleware/index';
+import startServer from './server';
+
+dotenv.config();
 const app = Express();
-const httpServer = new http.Server(app);
 
-httpServer.listen(3000, () => {
-    console.log('Start server covid API');
+loadMiddlewares(app);
+startServer(app);
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('<h1>Covid API!</h1>');
 });
