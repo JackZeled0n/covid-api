aa11a6e4ce3d2612ff571fada6d6d88dd50da8e2
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2021-05-05 23:08:11 -0600

    Add winston logger configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import http from 'http';
 import { variables } from './environment/variables';
+import log from './lib/logger';
 
 export default (app: Express.Application) => {
     const httpServer = new http.Server(app);
     httpServer.listen(variables.SERVER_PORT, () => {
-        console.log('Start server covid API');
+        log.info(`Listening on port: ${variables.SERVER_PORT}`);
     });
     process.on('SIGTERM', () => {
         httpServer.close(() => {
