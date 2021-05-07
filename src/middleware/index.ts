54f7ea1889dafd2882b5d0f1419e832d0dcdc3fc
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2021-05-07 01:18:27 -0600

    Add error middleware and update cors middleware reference

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
-import loadCorsConfiguration from './cors-configuration';
+import loadCorsConfiguration from './cors.middleware';
 
 export default (app: express.Application) => {
     app.use(morgan('tiny'));
