a5284e13b0861b05573885b507c28fa2abaac431
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2021-05-07 01:14:30 -0600

    Add new rules to eslint configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
     },
     rules: {
         'no-console': 'warn',
+        'class-methods-use-this': ['error', { exceptMethods: ['addNewDeaths', 'addNewCases', 'addNewTests'] }],
+        'no-param-reassign': ['off'],
     },
 };
